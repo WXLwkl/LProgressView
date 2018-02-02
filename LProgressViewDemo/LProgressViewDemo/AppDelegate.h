db6d08c5a6efd9eaa20b15658a494f7a36d64c5e
xingl <935858549@qq.com>
2018-02-02 13:42:55 +0800

    add LProgressView and LProgressView Demo

---
+++ LProgressViewDemo/LProgressViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LProgressViewDemo
+//
+//  Created by xingl on 2018/2/2.
+//  Copyright © 2018年 xingl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
